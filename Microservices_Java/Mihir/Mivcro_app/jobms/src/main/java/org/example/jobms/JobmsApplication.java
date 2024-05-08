c61000c643e5ed9fc600f4c88628c43358d3970d
mihirh19 <miheerhadvani990@gmail.com>
2024-05-08 22:37:09 +0530

    Add jobms module with initial files

---
+++ Microservices_Java/Mihir/Mivcro_app/jobms/src/main/java/org/example/jobms/JobmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmsApplication.class, args);
+	}
+
+}
---
+++ Microservices_Java/Mihir/Mivcro_app/jobms/src/main/java/org/example/jobms/job/Job.java
@@ -0,0 +1,91 @@
+package org.example.jobms.job;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Job {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+    private Long companyId;
+
+    public Job(Long id, String title, String description, String minSalary, String maxSalary, String location, Long companyId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+        this.companyId = companyId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Job() {
+    }
+}
