3e1f925d7d36d08ef518e2d1c055ae872898ba78
NisargPipaliya <nisargpipaliya2402@gmail.com>
2024-08-07 17:42:11 +0530

    Rearrangement in Files and Folders

--- Microservices_Java/Mihir/Mivcro_app/jobms/src/main/java/org/example/jobms/job/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.jobms.job;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
