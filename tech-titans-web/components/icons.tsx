8059420da8e37bbd0506c5bf953e1005872071a2
omjogani <om080jogani@gmail.com>
2024-08-10 17:20:48 +0530

    Added: next-theme | Implemented Navbar with light/dark mode

---
+++ tech-titans-web/components/icons.tsx
@@ -0,0 +1,10 @@
+// Sun Light mode Icon
+import { IoSunnyOutline } from "react-icons/io5";
+
+// Moon Dark mode Icon
+import { IoMoonOutline } from "react-icons/io5";
+
+// Books Icon
+import { FaBookReader } from "react-icons/fa";
+
+export { FaBookReader, IoSunnyOutline, IoMoonOutline };
---
+++ tech-titans-web/components/navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import { ModeToggle } from "@/components/theme-toggle";
+import { FaBookReader } from "@/components/icons";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="align-center flex items-center justify-between bg-white p-4 shadow-md dark:bg-gray-800">
+      <div className="flex flex-row content-center text-xl font-bold">
+        <FaBookReader className="mr-5 text-3xl" />
+        <Link
+          href="/"
+          className="text-blue-500 contain-content dark:text-blue-300"
+        >
+          Tech Titans
+        </Link>
+      </div>
+      <div className="flex space-x-4">
+        <Link
+          href="/"
+          className="content-center text-gray-700 dark:text-gray-300"
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="content-center text-gray-700 dark:text-gray-300"
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className="content-center text-gray-700 dark:text-gray-300"
+        >
+          Contact
+        </Link>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
