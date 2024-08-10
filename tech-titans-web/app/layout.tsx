efcec5b342c6f3e1b725f18a39d5b30801e0aad9
omjogani <om080jogani@gmail.com>
2024-08-10 15:53:38 +0530

    Added: ShadCn button | Minor Changes

--- tech-titans-web/app/layout.tsx
+++ tech-titans-web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
